d4e3d532f782dd4238c7d03b7fd58707cd61c1e1
OmkarGurav <omkargurav1920@gmail.com>
2023-04-05 17:24:28 +0530

    Fix: darkMode toggler

--- src/components/DarkToggler.jsx
+++ src/components/DarkToggler.jsx
@@ -3,45 +3,22 @@ import { BsSunFill, BsFillMoonFill } from 'react-icons/bs'
 
 function DarkToggler() {
 
-    const [theme, setTheme] = useState(null);
-
-    // toggler 
-    const [isSelected, setIsSelected] = useState(false);
-
-
-
-
-    useState(() => {
-        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            setTheme('dark')
-        }
-        else {
-            setTheme('light')
-        }
-    }, []);
-
-
+    const [isDarkMode, setIsDarkMode] = useState(true);
 
     useEffect(() => {
-        if (localStorage.getItem('theme') === 'light') {
-            document.documentElement.classList.remove('dark');
-        } else {
-            document.documentElement.classList.add('dark');
-            setIsSelected(true)
+        const currentMode = localStorage.getItem('mode');
+        if (currentMode === 'dark') {
+          setIsDarkMode(true);
+          document.documentElement.classList.add('dark');
         }
 
-    }, [theme])
+    }, [])
 
     const handlethemeSwitch = () => {
-
-        localStorage.setItem('theme', theme)
-        setTheme(theme === "dark" ? "light" : "dark");
-
-        // const setThemeInStorage = (selected) => {
-        //     localStorage.setItem('isSelected', isSelected)
-        // }
-        setIsSelected(!isSelected);
-
+        setIsDarkMode(!isDarkMode);
+        const mode = isDarkMode ? 'light' : 'dark';
+        localStorage.setItem('mode', mode);
+        document.documentElement.classList.toggle('dark');
     }
 
     return (
@@ -49,9 +26,9 @@ function DarkToggler() {
 
             {/* // toggler */}
             <div onClick={handlethemeSwitch} className=' flex w-20 h-10 bg-indigo-500 dark:bg-gray-600  m-10 rounded-full '>
-                <span className={isSelected ? 'h-10 w-10 bg-white rounded-full transition-all duration-500 items-center' : 'h-10 w-10 bg-[#171E29]  rounded-full ml-10 transition-all duration-500 '}>
+                <span className={isDarkMode ? 'h-10 w-10 bg-white rounded-full transition-all duration-500 items-center' : 'h-10 w-10 bg-[#171E29]  rounded-full ml-10 transition-all duration-500 '}>
                     <div className=' flex text-center items-center mt-3 ml-3 '>
-                        {isSelected ? <BsFillMoonFill color='black' /> : <BsSunFill color='white' />}
+                        {isDarkMode ? <BsFillMoonFill color='black' /> : <BsSunFill color='white' />}
                     </div>
 
                 </span>
